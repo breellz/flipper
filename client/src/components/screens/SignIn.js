6624482dc48e534b67dc1b8db4a2cfcee6b00c49
Bassit owolabi <barseetbrown@gmail.com>
2022-03-06 12:27:31 +0100

    misc: header font

--- client/src/components/screens/SignIn.js
+++ client/src/components/screens/SignIn.js
@@ -7,13 +7,13 @@ const Home = () => {
     return (
         <div className='signin-card'>
             <div className="card signin-auth-card input-field">
-                <h2>Flipper</h2>
+                <h2 id='header-text'>Flipper</h2>
                 <p>Buy and sell faster</p>
                 <input type="text" placeholder="email" />
                 <input type="password" placeholder="password" />
                 <button className="btn waves-effect waves-light #6a1b9a purple darken-3">Sign In
                 </button>
-                <h6> <Link to='/signup'>Don't have an account? Sign up instead</Link></h6>
+                <h6> Don't have an account? <Link to='/signup'>Sign up</Link></h6>
             </div>
         </div>
     )
--- client/src/components/screens/SignUp.js
+++ client/src/components/screens/SignUp.js
@@ -7,7 +7,7 @@ const Home = () => {
     return (
         <div className='signup-card'>
             <div className="card signup-auth-card input-field">
-                <h2>Flipper</h2>
+                <h2 id='header-text'>Flipper</h2>
                 <p>Buy and sell faster</p>
                 <input type="text" placeholder="First Name" />
                 <input type="text" placeholder="Last Name" />
@@ -16,7 +16,7 @@ const Home = () => {
                 <input type="password" placeholder="password" />
                 <button className="btn waves-effect waves-light #6a1b9a purple darken-3">Sign Up
                 </button>
-                <h6> <Link to='/signin'>Already have an account? Sign in instead</Link></h6>
+                <h6> Already have an account?<Link to='/signin'> Sign In</Link></h6>
             </div>
         </div>
     )
