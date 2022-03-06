3d45aa8c47482d904bed67d649707508bbca3855
Bassit owolabi <barseetbrown@gmail.com>
2022-03-06 02:31:17 +0100

    feat: added user routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,7 +5,7 @@ const userAuth = require('../middleware/userAuth')
 const router = new express.Router()
 
 //Sign up route
-router.post('/users', async(req, res) => {
+router.post('/users', async (req, res) => {
     const user = new User(req.body)
     try {
         await user.save()
@@ -16,13 +16,73 @@ router.post('/users', async(req, res) => {
 })
 
 //login route
-router.post('/users/login', async(req, res) => {
+router.post('/users/login', async (req, res) => {
     try {
         const user = await User.findByCredentials(req.body.email, req.body.password)
         const token = await user.generateAuthToken()
-        res.send({ user, token})
+        res.send({ user, token })
     } catch (error) {
-        res.status(400).send({ error: error.message})
+        res.status(400).send({ error: error.message })
+    }
+})
+
+//fetch my profile 
+
+router.get('/users/me', userAuth, (req, res) => {
+    try {
+        res.send(req.user)
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+
+//fetch a user profile
+
+router.get('/users/:userId', async (req, res) => {
+    try {
+        const userId = req.params.userId
+        const user = await User.findOne({ _id: userId })
+        if (!user) {
+           return res.status(404).send({ error: "no user was found" })
+        }
+        res.send(user)
+    } catch (error) {
+        res.status(400).send()
+    }
+})
+
+//update user profile
+
+router.patch('/users/me', userAuth, async(req, res) => {
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['firstName', 'lastName','email','password']
+
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'invalid updates' })
+    }
+    try {
+        const user = req.user
+
+        updates.forEach((update) => user[update] = req.body[update])
+
+        await user.save()
+        res.send(user)
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
+//delete user 
+
+router.delete('/users/me', userAuth, async(req, res) => {
+    try {
+        await req.user.remove()
+       // sendCancellationEmail(req.user.email, req.user.name)
+        res.send(req.user)
+    } catch (error) {
+        res.status(400).send()        
     }
 })
 
